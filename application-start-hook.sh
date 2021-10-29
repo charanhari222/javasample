6f975de0fe2bef1332b358b251456638bb9a5c13
charanhari222 <79359215+charanhari222@users.noreply.github.com>
2021-10-29 18:02:27 +0530

    Update application-start-hook.sh

--- application-start-hook.sh
+++ application-start-hook.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-aws s3 cp s3://codepipeline-ap-south-1-592199801684/sample/buildartif/wss.war /tmp
-sudo mv /tmp/wss.war apache-tomcat-9.0.54/webapps/ROOT.war
-sudo service tomcat restart
+sudo mv *.war apache-tomcat-9.0.54/webapps/
+sudo apache-tomcat-9.0.54/bin/startup.sh
