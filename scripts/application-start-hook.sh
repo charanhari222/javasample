2e6e79f4687ceabb72d55cd99dfaea412263ee2d
charanhari222 <79359215+charanhari222@users.noreply.github.com>
2021-10-29 16:28:00 +0530

    Delete scripts directory

--- scripts/application-start-hook.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-aws s3 cp s3://codepipeline-ap-south-1-592199801684/sample/buildartif/q2TbGkl.war /temp
-sudo mv /temp/q2TbGkl.war apache-tomcat-9.0.54/webapps/ROOT.war
-sudo apache-tomcat-9.0.54/bin/shutdown.sh
-sudo apache-tomcat-9.0.54/bin/startup.sh
